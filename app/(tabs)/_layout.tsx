15ff7fc9cf87becf242a154b413bc695e121b3d6
jeffmlb91 <jeffmlb91@gmail.com>
2023-12-15 21:44:16 -0500

    add icon in _layout.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,54 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 
 const Layout = () => {
   return (
-    <Tabs>
-        
+    <Tabs screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+            fontFamily: "mon-sb",
+        },
+    }}>
+        <Tabs.Screen  
+            name="index"
+            options={{
+                tabBarLabel: 'Explore',
+                tabBarIcon: ({color, size}) => <Ionicons name="search" size={size} color={color} />
+            }}
+        />
+        <Tabs.Screen  
+            name="wishlists"
+            options={{
+                tabBarLabel: 'Wishlists',
+                tabBarIcon: ({color, size}) => <Ionicons name="heart-outline" size={size} color={color} />
+            }}
+        />
+         <Tabs.Screen  
+            name="trips"
+            options={{
+                tabBarLabel: 'Trips',
+                tabBarIcon: ({color, size}) => <FontAwesome5 name="airbnb" size={size} color={color} />
+            }}
+        />
+        <Tabs.Screen  
+            name="inbox"
+            options={{
+                tabBarLabel: 'Inbox',
+                tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="message-outline" size={size} color={color} />
+            }}
+        />
+         <Tabs.Screen  
+            name="profile"
+            options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({color, size}) => <Ionicons name="person-circle-outline" size={size} color={color} />
+            }}
+        />
     </Tabs>
   )
 }
